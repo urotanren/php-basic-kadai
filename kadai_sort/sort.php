11dfd2325f77476c8f2557332b3aa27f97986c29
urotanren <148876227+urotanren@users.noreply.github.com>
2023-12-15 21:47:03 +0900

    kadai_sortをアップロードしました。

---
+++ kadai_sort/sort.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        
+
+        function sort_2way($array,$order){
+
+            $array = [15,4,18,23,10];
+
+            if($order == TRUE){
+                sort($array);
+                echo "昇順にソートします。";
+                echo "<br>";
+                foreach($array as $key => $value){
+                    echo "{$value}<br>";
+                };
+            }elseif($order == FALSE){
+                rsort($array);
+                echo "降順にソートします。";
+                echo "<br>";
+                foreach($array as $key => $value){
+                    echo "{$value}<br>";
+                };
+            };
+        };
+
+        sort_2way($array,TRUE);
+        sort_2way($array,FALSE);
+        ?>
+    </p>
+</body>
+
+</html>
